54f81c05186630f9ba39725e89badf3a6bde4af1
Whitemoons <alrnd01@snu.ac.kr>
2025-04-01 01:05:02 +0900

    solved: 2747 - 피보나치 수

---
+++ 02000/2747.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int dp[46] = {0,};
+
+    dp[1] = 1;
+    dp[2] = 1;
+
+    scanf("%d", &n);
+    
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    printf("%d", dp[n]);
+
+    return 0;
+}
